bc14564381116efbc25052edf57075ad199dc9c8
= <=>
2022-10-16 11:45:45 +0200

    initialized viewmodel

--- app/src/main/java/com/salma/daggerhiltbasics/MainActivity.kt
+++ app/src/main/java/com/salma/daggerhiltbasics/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.salma.daggerhiltbasics.ui.theme.DaggerHiltBasicsTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +23,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-
+                    val viewModel = hiltViewModel<MyViewModel>()
                 }
             }
         }
